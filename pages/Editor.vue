5a72ce9efa3dabb677723df5a0e0f3e298512a8f
Hugo BOIS <hugo.bois@epita.fr>
2022-01-06 01:00:19 +0100

    added editor

---
+++ pages/Editor.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="editor">
+    <Navbar />
+    <Editor />
+    <HomePageFooter />
+  </div>
+</template>
+
+<script lang="ts">
+import Navbar from "@/components/HomePageNavbar.vue";
+import Editor from "@/components/Editor.vue";
+
+export default {
+  components: {
+    Navbar,
+    Editor,
+  },
+  head: {
+    title: "Scriptio - Editor",
+  },
+};
+</script>
