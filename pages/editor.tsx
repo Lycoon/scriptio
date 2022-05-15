8fd85650e2f748c903fc635a5798d49abfec6d00
Hugo Bois <hugo.bois@hotmail.fr>
2022-05-15 21:57:22 +0200

    add: user for editor

--- pages/editor.tsx
+++ pages/editor.tsx
@@ -1,20 +1,51 @@
+import { withIronSessionSsr } from "iron-session/next";
 import type { NextPage } from "next";
 import Head from "next/head";
 import EditorContainer from "../components/editor/EditorContainer";
 import HomePageNavbar from "../components/navbar/Navbar";
+import { sessionOptions } from "../src/lib/session";
+import { User } from "./api/users";
 
-const EditorPage: NextPage = () => {
+const EditorPage: NextPage<{ user: User }> = ({ user }) => {
   return (
     <div>
       <Head>
         <title>Scriptio - Editor</title>
       </Head>
       <div className="main-container">
-        <HomePageNavbar className="navbar" />
+        <HomePageNavbar className="navbar" user={user} />
         <EditorContainer />
       </div>
     </div>
   );
 };
+
+export const getServerSideProps = withIronSessionSsr(async function ({
+  req,
+  res,
+}) {
+  const user = req.session.user;
+
+  if (user === undefined) {
+    /*res.setHeader("location", "/");
+    res.statusCode = 302;
+    res.end();*/
+
+    return {
+      props: {
+        user: {
+          isLoggedIn: false,
+          email: "",
+          id: -1,
+        },
+      },
+    };
+  }
+
+  return {
+    props: { user: req.session.user },
+  };
+},
+sessionOptions);
 
 export default EditorPage;
