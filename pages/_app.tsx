0848028e2c7c2e8d12b6630449ff25a714bc8341
Hugo Bois <hugo.bois@hotmail.fr>
2022-05-26 11:54:59 +0200

    add: _document.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,46 +3,22 @@ import type { AppProps } from "next/app";
 import { ContextProvider } from "../src/context/UserContext";
 import { SWRConfig } from "swr";
 import fetchJson from "../src/lib/fetchJson";
-import Head from "next/head";
-
-const DESCRIPTION =
-  "Imagine, tell, amaze. Scriptio is your screenwriting companion designed with simplicity in mind and no frills.";
-const TITLE = "Scriptio | Minimalist tool for perfectionist screenwriters";
-const TITLE_IMG = "https://scriptio.app/assets/homepage.png";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <Head>
-        <meta name="title" content={TITLE} />
-        <meta name="description" content={DESCRIPTION} />
-
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://scriptio.app/" />
-        <meta property="og:title" content={TITLE} />
-        <meta property="og:description" content={DESCRIPTION} />
-        <meta property="og:image" content={TITLE_IMG} />
-
-        <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:url" content="https://scriptio.app/" />
-        <meta property="twitter:title" content={TITLE} />
-        <meta property="twitter:description" content={DESCRIPTION} />
-        <meta property="twitter:image" content={TITLE_IMG} />
-      </Head>
-      <SWRConfig
-        value={{
-          onSuccess: () => {},
-          fetcher: fetchJson,
-          onError: (err) => {
-            console.error(err);
-          },
-        }}
-      >
-        <ContextProvider>
-          <Component {...pageProps} />
-        </ContextProvider>
-      </SWRConfig>
-    </>
+    <SWRConfig
+      value={{
+        onSuccess: () => {},
+        fetcher: fetchJson,
+        onError: (err) => {
+          console.error(err);
+        },
+      }}
+    >
+      <ContextProvider>
+        <Component {...pageProps} />
+      </ContextProvider>
+    </SWRConfig>
   );
 }
 
---
+++ pages/_document.tsx
@@ -0,0 +1,45 @@
+import NextDocument, { Html, Head, Main, NextScript } from "next/document";
+import React from "react";
+
+const DESCRIPTION =
+  "Imagine, tell, amaze. Scriptio is your screenwriting companion designed with simplicity in mind and no frills.";
+const TITLE = "Scriptio | Minimalist tool for perfectionist screenwriters";
+const TITLE_IMG = "https://scriptio.app/assets/homepage.png";
+
+type Props = {};
+
+class Document extends NextDocument<Props> {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <meta name="title" content={TITLE} />
+          <meta name="description" content={DESCRIPTION} />
+          <meta name="author" content="Hugo 'Lycoon' Bois" />
+          <meta
+            name="keywords"
+            content="movie, script, writing, story, storywriting, screenwriting, screenwriter"
+          />
+
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://scriptio.app/" />
+          <meta property="og:title" content={TITLE} />
+          <meta property="og:description" content={DESCRIPTION} />
+          <meta property="og:image" content={TITLE_IMG} />
+
+          <meta property="twitter:card" content="summary_large_image" />
+          <meta property="twitter:url" content="https://scriptio.app/" />
+          <meta property="twitter:title" content={TITLE} />
+          <meta property="twitter:description" content={DESCRIPTION} />
+          <meta property="twitter:image" content={TITLE_IMG} />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default Document;
