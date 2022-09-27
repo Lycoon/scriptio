700f1d5d2aae919997062ce0994aabfe26718739
Hugo Bois <hugo.bois@hotmail.fr>
2022-09-27 14:18:40 +0200

    fixed compilation

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,8 +1,6 @@
-import type { NextPage } from "next";
 import Head from "next/head";
 import AboutPageContainer from "../components/home/AboutPageContainer";
 import Navbar from "../components/navbar/Navbar";
-import { User } from "./api/users";
 
 const AboutPage = () => (
     <>
---
+++ pages/contact.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+import Navbar from "../components/navbar/Navbar";
+
+const ContactPage = () => (
+    <>
+        <Head>
+            <title>Scriptio - Contact</title>
+        </Head>
+        <div className="main-container">
+            <Navbar />
+        </div>
+    </>
+);
+
+export default ContactPage;
