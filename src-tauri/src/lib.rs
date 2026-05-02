a7bfc5a46aa373add8a68666330750541c4cae4f
Hugo <hugo@device-35.home>
2026-05-02 13:24:30 +0200

    fixed clipboard handling and web search for desktop apps

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -6,6 +6,7 @@ fn some_noop_command() {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_clipboard_manager::init())
         .plugin(tauri_plugin_store::Builder::default().build())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_fs::init())
